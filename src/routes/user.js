2cef5a89f2340d54fa131458854f170d0d852834
Matias Soto <matias.soto.26@gmail.com>
2023-05-03 09:42:02 -0400

    Full CRUD added

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,18 +7,45 @@ const router = express.Router();
 router.post('/user', (req, res) => {
     const user = User(req.body);
     user
-    .save()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error}));
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
 });
 
 // GET ALL
 router.get('/user', (req, res) => {
     User
-    .find()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error}));
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
 });
 
+// GET BY ID
+router.get('/user/:id', (req, res) => {
+    const { id } = req.params;
+    User
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+// UPDATE
+router.put('/user/:id', (req, res) => {
+    const { id } = req.params;
+    const { nombre, apellido, email, password, tipo } = req.body;
+    User
+        .updateOne({ _id: id }, { $set: { nombre, apellido, email, password, tipo } })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+// DELETE
+router.delete('/user/:id', (req, res) => {
+    const { id } = req.params;
+    User
+        .deleteOne({ _id: id })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
 module.exports = router;
